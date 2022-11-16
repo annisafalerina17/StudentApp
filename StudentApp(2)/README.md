cde64539aed0addc3cc7d097f654a8741067d369
Sri Annisa Falerina H <74857839+annisafalerina17@users.noreply.github.com>
2022-11-16 11:01:06 +0700

    Add files via upload

---
+++ StudentApp(2)/README.md
@@ -0,0 +1,4 @@
+Team Members :
+1. Ni Luh Ari Yanika (1313620003)
+2. Sri Annisa Falerina (1313620025)
+3. Nada Razani (1313620042)
---
+++ StudentApp(2)/app/src/main/java/unj/cs/student/StudentListFragment.kt
@@ -0,0 +1,63 @@
+package unj.cs.student
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import unj.cs.student.databinding.FragmentStudentListBinding
+
+class StudentListFragment : Fragment() {
+    private var _binding: FragmentStudentListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var toastMessage: String
+    private val viewModel: StudentViewModel by activityViewModels {
+        StudentViewModelFactory(
+            (activity?.application as StudentApplication).database.studentDao()
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStudentListBinding.inflate(inflater, container, false)
+        val root : View = binding.root
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = StudentAdapter{
+            val action: NavDirections = StudentListFragmentDirections.actionStudentListFragmentToStudentFormFragment(it.id, "Update Student")
+            view.findNavController().navigate(action)
+        }
+        binding.studentViewRecycler.adapter = adapter
+        viewModel.allStudents.observe(this.viewLifecycleOwner) {
+                items -> items.let { adapter.submitList(it) }
+        }
+        val addButton : FloatingActionButton = binding.addButton
+
+        addButton.setOnClickListener{
+            val action: NavDirections = StudentListFragmentDirections.actionStudentListFragmentToStudentFormFragment(-1, "Add Student")
+            view.findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
